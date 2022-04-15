06b8218fb9df41ef406d8c10d0b1e633ab81226d
Nikita <nikita.pathak1608@gmail.com>
2022-04-16 00:24:34 +0530

    added all the possible stuff

---
+++ Add.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Add {
+
+	public static void main(String[] args) {
+		int a,b=0;
+		a=3;
+		b=10;
+		int sum=a+b;
+		System.out.println("sum of "+a+" and "+b+" is: "+sum);
+	}
+
+}
